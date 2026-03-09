c0510c3d76577aa32b471573c07193a05e406b8d
Britneymuthoni67 <peters.beau8@gmail.com>
2026-03-09 20:41:07 +0300

    Add basic arithmetic and logical operations

---
+++ operators.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 3;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Remainder:" a % b);
+
+console.log("Is a greater than b?", a > b);
+console.log("Is a equal to b?", a == b);
+
+let isSunny = true;
+let isWeekend = false;
+console.log("Can we go out?", isSunny && isWeekend);
